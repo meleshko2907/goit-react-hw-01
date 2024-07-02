6de904fe08167c4e98b2a82881aced9a0f06334b
Meleshko Aleksey <110394498+meleshko2907@users.noreply.github.com>
2024-07-02 21:48:47 +0200

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 // import PropTypes from 'prop-types';
 import "modern-normalize";
-import App from '../App';
+import App from './App';
 
 
 
@@ -11,5 +11,5 @@ import App from '../App';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
